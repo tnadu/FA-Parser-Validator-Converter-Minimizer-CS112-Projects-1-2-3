deb7942e631c177185ae956ee46f5c87446fb4f3
Toma Nadu <toma.nadu@protonmail.com>
2022-02-28 14:08:21 +0200

    DFA - Improved format error detection

--- DFA Parser.py
+++ DFA Parser.py
@@ -14,7 +14,7 @@ S = ''
 with open(sys.argv[1]) as f:
     lines = f.readlines()
 
-    if not (lines):
+    if not lines:
         print('Fatal error: Config file cannot be empty.')
         quit()
 
@@ -24,68 +24,72 @@ with open(sys.argv[1]) as f:
             print('Format error: Lines cannot be empty.')
             quit()
 
-        if lines[index][0] != '#':
-            if 'Sigma' == ''.join(spl('[:\s]+', lines[index])):
+        elif lines[index][0] != '#':
+            if 'Sigma:' == lines[index].strip():
                 index += 1
                 while index < len(lines) and 'End' != lines[index].strip():
-                    if lines[index][0] == '#' or not (lines[index].strip()):
-                        print('Format error: Section "Sigma" must end with "End" line.')
-                        quit()
-                    if not (lines[index].strip().isalnum()):
-                        print('Format error: Section "Sigma" can only contain alphanumerical values.')
-                        quit()
+                    if lines[index][0] != '#':
+                        if lines[index].strip() in {'', 'States:', 'Transitions:'}:
+                            print('Format error: Section "Sigma" must end with "End" line.')
+                            quit()
+                        elif not (lines[index].strip().isalnum()):
+                            print('Format error: Section "Sigma" can only contain alphanumerical values.')
+                            quit()
+                        sigma.append(lines[index].strip())
 
-                    sigma.append(lines[index].strip())
                     index += 1
                 index += 1
 
-            elif 'States' == ''.join(spl('[:\s]+', lines[index])):
+            elif 'States:' == lines[index].strip():
                 index += 1
                 while index < len(lines) and 'End' != lines[index].strip():
-                    if lines[index][0] == '#' or not (lines[index].strip()):
-                        print('Format error: Section "States" must end with "End" line.')
-                        quit()
-
-                    state = lines[index].strip().rstrip(" ,F").rstrip(" ,S")
-                    if not (state.strip().isalnum()):
-                        print('Format error: Section "States" can only contain alphanumerical values.')
-                        quit()
-
-                    states.append(state)
-                    if ',F' in lines[index]:
-                        F.append(state)
-                    if ',S' in lines[index]:
-                        if S != '':
-                            print(
-                                'Condition error: There can only be one initial state.')
+                    if lines[index][0] != '#':
+                        if lines[index].strip() in {'', 'Sigma:', 'Transitions:'}:
+                            print('Format error: Section "States" must end with "End" line.')
                             quit()
-                        else:
-                            S = state
+
+                        state = lines[index].strip().rstrip(" ,F").rstrip(" ,S")
+                        if not (state.strip().isalnum()):
+                            print('Format error: Section "States" can only contain alphanumerical values.')
+                            quit()
+
+                        states.append(state)
+                        if ',F' in lines[index]:
+                            F.append(state)
+                        if ',S' in lines[index]:
+                            if S != '':
+                                print('Condition error: There can only be one initial state.')
+                                quit()
+                            else:
+                                S = state
+
                     index += 1
                 index += 1
 
-            elif 'Transitions' == ''.join(spl('[:\s]+', lines[index])):
+            elif 'Transitions:' == lines[index].strip():
                 index += 1
                 while index < len(lines) and 'End' != lines[index].strip():
-                    if lines[index][0] == '#' or not (lines[index].strip()):
-                        print('Format error: Section "Transitions" must end with "End" line.')
-                        quit()
-
-                    currentTransition = tuple([x for x in spl('[,\s]+', lines[index]) if x])
-                    if len(currentTransition) != 3:
-                        print('Format error: Section "Transitions" must contain 3 values for each line.')
-                        quit()
-                    for transition in currentTransition:
-                        if not (transition.strip().isalnum()):
-                            print('Format error: Section "Transitions" can only contain alphanumerical values.')
+                    if lines[index][0] != '#':
+                        if lines[index].strip() in {'', 'Sigma:', 'States:'}:
+                            print('Format error: Section "Transitions" must end with "End" line.')
                             quit()
 
-                    transitions.append(currentTransition)
+                        currentTransition = tuple([x for x in spl('[,\s+]+', lines[index]) if x])
+                        if len(currentTransition) != 3:
+                            print('Format error: Section "Transitions" must contain 3 values for each line.')
+                            quit()
+                        for transition in currentTransition:
+                            if not (transition.strip().isalnum()):
+                                print('Format error: Section "Transitions" can only contain alphanumerical values.')
+                                quit()
+
+                        transitions.append(currentTransition)
+
                     index += 1
                 index += 1
 
             else:
-                print('Format error: Config file can only contain "Sigma", "States" and "Transitions" sections.')
+                print('Format error: Config file can only contain "Sigma:", "States:" and "Transitions:" sections.')
                 quit()
 
         else:
