31100722283f575d781aec9d33f5ba6068ddeb6c
Toma Nadu <toma.nadu@protonmail.com>
2022-03-22 13:48:23 +0200

    Program menu and NFA config file validation completed

--- FA-Parser-Validator-Converter.py
+++ FA-Parser-Validator-Converter.py
@@ -42,7 +42,7 @@ with open(sys.argv[1]) as f:
                             print('Format error: Section "Sigma" must end with "End" line')
                             quit()
                         elif not (
-                        lines[index].strip().isalnum()):  # only alphanumerical characters allowed for declaring letters
+                                lines[index].strip().isalnum()):  # only alphanumerical characters allowed for declaring letters
                             print('Format error: Section "Sigma" can only contain alphanumerical values')
                             quit()
                         sigma.append(lines[index].strip())
@@ -99,10 +99,6 @@ with open(sys.argv[1]) as f:
                         if len(currentTransition) != 3:
                             print('Format error: Section "Transitions" must contain 3 values for each line')
                             quit()
-                        for transition in currentTransition:
-                            if not (transition.strip().isalnum()):
-                                print('Format error: Section "Transitions" can only contain alphanumerical values')
-                                quit()
 
                         transitions.append(currentTransition)
 
@@ -128,38 +124,49 @@ def index(element, List=states):
 # initially, each value within it is null
 T = [["null" for i in range(len(states))] for i in range(len(states))]
 
-for transition in transitions:
-    # verifies that all elements inside each tuple are valid members within states and sigma
-    if transition[0] not in states:
-        print(f'\'{transition[0]}\' not a valid state')
-        quit()
-    if transition[1] not in sigma:
-        print(f'\'{transition[1]}\' not a valid letter')
-        quit()
-    if transition[2] not in states:
-        print(f'\'{transition[2]}\' not a valid state')
-        quit()
+print('Select the type of finite automaton:')
+print('1) deterministic')
+print('2) nondeterministic')
+command = int(input('>>> '))
 
-    state1, letter, state2 = index(transition[0]), transition[1], index(transition[2])
-    if letter in T[state1]:         # testing for determinism; we check if current letter is already an element of T[state1]
-        print("Condition error: DFA must have unique transition letters for each state")
-        quit()
-    else:                           # registers the current tuple
-        T[state1][state2] = letter
+while command > 2 or command < 1:
+    print(f'Error: \'{command}\' not a valid option')
+    command = int(input('>>> '))
 
-print("Analyzed DFA is valid!")
+# DFA menu
+if command == 1:
+    for transition in transitions:
+        # verifies that all elements inside each tuple are valid members within states and sigma
+        if transition[0] not in states:
+            print(f'\'{transition[0]}\' not a valid state')
+            quit()
+        if transition[1] not in sigma:
+            print(f'\'{transition[1]}\' not a valid letter')
+            quit()
+        if transition[2] not in states:
+            print(f'\'{transition[2]}\' not a valid state')
+            quit()
 
-if len(sys.argv) == 3:
-    print("Checking validity for the given word...")
+        state1, letter, state2 = index(transition[0]), transition[1], index(transition[2])
+        if letter in T[state1]:  # testing for determinism; we check if current letter is already an element of T[state1]
+            print("Condition error: DFA must have unique transition letters for each state")
+            quit()
+        else:  # registers the current tuple
+            T[state1][state2] = letter
+
+    print("Analyzed DFA is valid!")
+
+    if len(sys.argv) == 3:
+        print("Checking validity for the given word...")
     # currentState ==> using this variable to navigate through each state
     currentState = 0
 
-    for i in range(len(word)):      # each letter of the given word is verified
-        stop = True                 # we assume that the word would not pass the verification test
+    for i in range(len(word)):  # each letter of the given word is verified
+        stop = True  # we assume that the word would not pass the verification test
         for j in range(len(states)):
             if T[currentState][j] == word[i]:
                 currentState = j
-                stop = False        # we continue the validation since the current letter was correct
+                stop = False  # we continue the validation since the current letter was correct
                 break
 
         if stop:
@@ -167,7 +174,73 @@ if len(sys.argv) == 3:
             quit()
             break
 
-    if not(stop) and currentState in [int(x) for x in F]:  # finally, we check that the final state is a member of F
+    if not stop and currentState in [int(x) for x in F]:  # finally, we check that the final state is a member of F
         print(f"The word '{word}' was accepted by the DFA!")
     else:
         print(f"The word '{word}' was not accepted by the DFA")
+
+
+# NFA menu
+elif command == 2:
+    for transition in transitions:
+        # verifies that all elements inside each tuple are valid members within states and sigma
+        if transition[0] not in states:
+            print(f'\'{transition[0]}\' not a valid state')
+            quit()
+        if transition[1] not in sigma and transition[1] != '*':
+            print(f'\'{transition[1]}\' not a valid letter')
+            quit()
+        if transition[2] not in states:
+            print(f'\'{transition[2]}\' not a valid state')
+            quit()
+
+        state1, letter, state2 = index(transition[0]), transition[1], index(transition[2])
+        T[state1][state2] = letter
+
+    print("Analyzed NFA is valid!")
+
+    if len(sys.argv) == 2:
+        print('Would you like to convert the current NFA to an equivalent DFA?')
+        print('Type \'1\' for \'yes\' or \'0\' for \'no\'')
+        command = int(input('>>> '))
+
+        while command > 1 or command < 0:
+            print(f'Error: \'{command}\' not a valid option')
+            command = int(input('>>> '))
+
+        if command == 0:
+            quit()
+        elif command == 1:
+            convertToDFA()  # this will be a function
+            quit()
+
+    else:
+        print('Type \'0\' to quit or select one or both of the following options: ')
+        print('1) convert NFA to DFA')
+        print('2) check word validity')
+        command = [int(x) for x in input('>>> ').split()]
+
+        check = True
+
+        while check:
+            check = False
+            if len(command) > 3:
+                print('Error: Too many options')
+                command = [int(x) for x in input('>>> ').split()]
+                check = True
+            else:
+                for num in command:
+                    if num > 3 or num < 3:
+                        print(f'Error: \'{num}\' is not a valid option')
+                        command = [int(x) for x in input('>>> ').split()]
+                        check = True
+                        break
+
+        if 0 in command:
+            quit()
+
+        for num in command:
+            if num == 1:
+                convertToDFA()  # function
+            elif num == 2:
+        # check word validity
