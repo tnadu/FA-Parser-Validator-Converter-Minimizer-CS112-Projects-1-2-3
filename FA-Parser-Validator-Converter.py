ce75a8264899f044c26b1da9634ed2f851ba48dc
Toma Nadu <toma.nadu@protonmail.com>
2022-03-24 15:15:49 +0200

    Progress on NFA conversion

--- FA-Parser-Validator-Converter.py
+++ FA-Parser-Validator-Converter.py
@@ -114,67 +114,108 @@ with open(sys.argv[1]) as f:
 
 
 # Function that returns the index of a given element in a list
-def getIndex(element, List=states):
-    for i in range(len(List)):
-        if element == List[i]:
+def getIndex(element):
+    for i in range(len(states)):
+        if element == states[i]:
             return i
 
-def convertToDFA():
-    states1 = [S]
-    newStates = {}
-    transitions1 = {}
-    F1 = [*F]
 
-    notationIndex = 0
-    i = 0
-    while i < len(states1):
-        if type(states1[i]) != set:
-            newStateTransitions = {}
-            for letter in sigma:
-                newState = []
-                finalState = False
+def convertToDFA():
+    states1 = [S]  # new DFA states
+    newStates = {}  # dictionary for keeping track of new states' names
+    transitions1 = {}  # new DFA transitions
+    F1 = [*F]  # new DFA final states
+
+    notationIndex = 0  # int variable for new states naming
+    index = 0  # variable for traversing new DFA states (states1)
+    # unless no more new states have been added and all previously added states have been checked, proceed
+    while index < len(states1):
+        # current DFA state is one of the original NFA's states
+        if type(states1[index]) != tuple:
+            newStateTransitions = {}  # dictionary for each DFA state, which will be nested in transitions1
+            for letter in sigma:  # for every letter of the alphabet, a new possible state will be created to store the states ->
+                newState = []  # that the current state transitions into
+                finalState = False  # bool variable to check whether newState contains an original final state
                 for i in range(len(states)):
-                    if T[getIndex(states1[i])][i] == letter:
-                        newState.append(sigma[i])  # getState receives index of state and returns state itself
-                        if sigma[i] in F:
+                    if T[getIndex(states1[index])][i] == letter:
+                        newState.append(states[i])
+                        if states[i] in F:  # newState contains an original final state
                             finalState = True
 
-                if newState:
-                    if set(newState) not in newStates:
-                        newStates[set(newState)] = f'new_state_{notationIndex}'
-                        notationIndex += 1
-                        if finalState:
-                            F1.append(set(newState))
-                    newStateTransitions[letter] = set(newState)
-                else:
-                    newStateTransitions[letter] = None
-            transitions1[states1[i]] = newStateTransitions
+                if newState:  # we check if any states have been found
+                    if len(newState) == 1:  # newState is an original NFA state
+                        newState = ''.join(newState)
+                        if newState not in states1:  # original NFA state is checked for in DFA states
+                            states1.append(newState)
+                        newStateTransitions[letter] = newState  # original NFA state - letter pair is added to current states1 state's transitions
 
+                    else:  # newState contains multiple original NFA states
+                        # check whether newState is an unstored state
+                        found = False
+                        for state in states1:
+                            if set(newState) == set(state):
+                                found = True
+                                break
+
+                        if not found:  # if newState is unstored ->
+                            newStates[tuple(newState)] = f'new_state_{notationIndex}'  # it is assigned a new name ->
+                            states1.append(tuple(newState))  # and it's stored in the DFA's states
+                            notationIndex += 1
+                            # newState contains an original NFA state, therefore it's a final state of the DFA
+                            if finalState:
+                                F1.append(set(newState))
+                        # original NFA state - letter pair is added to current states1 state's transitions
+                        newStateTransitions[letter] = tuple(newState)
+                else:  # newState is empty, so current DFA state doesn't transition into any other state
+                    newStateTransitions[letter] = None
+            transitions1[states1[index]] = newStateTransitions
+
+        # current DFA state isn't an original NFA state, therefore it's composed
         else:
-            newStateTransitions = {}
-            for letter in sigma:
-                newState = []
-                finalState = False
-                for state in states1[i]:
+            newStateTransitions = {}  # dictionary for each DFA state, which will be nested in transitions1
+            for letter in sigma:  # for every letter of the alphabet, a new possible state will be created to store the states ->
+                newState = []  # that the current state transitions into
+                finalState = False  # bool variable to check whether newState contains an original final state
+                # all component states' transitioned states will be added to newState
+                for state in states1[index]:
                     for i in range(len(states)):
                         if T[getIndex(state)][i] == letter:
-                            newState.append(sigma[i])  # getState receives index of state and returns state itself
-                            if sigma[i] in F:
+                            if states[i] not in newState:
+                                newState.append(states[i])
+                            if states[i] in F:  # newState contains an original final state
                                 finalState = True
 
-                if newState:
-                    if set(newState) not in newStates:
-                        newStates[set(newState)] = f'new_state_{notationIndex}'
-                        notationIndex += 1
-                        if finalState:
-                            F1.append(set(newState))
-                    newStateTransitions[letter] = set(newState)
-                else:
+                if newState:  # we check if any states have been found
+                    if len(newState) == 1:  # newState is an original NFA state
+                        newState = ''.join(newState)
+                        if newState not in states1:  # original NFA state is checked for in DFA states
+                            states1.append(newState)
+                        newStateTransitions[letter] = newState  # original NFA state - letter pair is added to current states1 state's transitions
+
+                    else:  # newState contains multiple original NFA states
+                        # check whether newState is an unstored state
+                        found = False
+                        for state in states1:
+                            if set(newState) == set(state):
+                                found = True
+                                break
+
+                        if not found:  # if newState is unstored ->
+                            newStates[tuple(newState)] = f'new_state_{notationIndex}'  # it is assigned a new name ->
+                            states1.append(tuple(newState))  # and it's stored in the DFA's states
+                            notationIndex += 1
+                            # newState contains an original NFA state, therefore it's a final state of the DFA
+                            if finalState:
+                                F1.append(set(newState))
+                        # original NFA state - letter pair is added to current states1 state's transitions
+                        newStateTransitions[letter] = tuple(newState)
+                else:  # newState is empty, so current DFA state doesn't transition into any other state
                     newStateTransitions[letter] = None
-            transitions1[states1[i]] = newStateTransitions
-        i += 1
+            transitions1[states1[index]] = newStateTransitions
+        index += 1
 
     return states1, newStates, transitions1, F1
+
 
 # T-list that stores each 3-tuple in transitions (state1, letter, state2) where T[state1_index][state2_index]=letter
 # initially, each value within it is null
@@ -255,6 +296,7 @@ elif command == 2:
 
     print("Analyzed NFA is valid!")
 
+    # no word received for validation
     if len(sys.argv) == 2:
         print('Would you like to convert the current NFA to an equivalent DFA?')
         print('Type \'1\' for \'yes\' or \'0\' for \'no\'')
@@ -271,14 +313,15 @@ elif command == 2:
             print(states1, newStates, transitions1, F1, sep='\n')
             quit()
 
+    # word received for validation
     else:
         print('Type \'0\' to quit or select one or both of the following options: ')
         print('1) convert NFA to DFA')
         print('2) check word validity')
         command = [int(x) for x in input('>>> ').split()]
-
         check = True
 
+        # command input format checking
         while check:
             check = False
             if len(command) > 3:
@@ -287,7 +330,7 @@ elif command == 2:
                 check = True
             else:
                 for num in command:
-                    if num > 3 or num < 3:
+                    if num > 2 or num < 0:
                         print(f'Error: \'{num}\' is not a valid option')
                         command = [int(x) for x in input('>>> ').split()]
                         check = True
@@ -302,4 +345,4 @@ elif command == 2:
                 print(states1, newStates, transitions1, F1, sep='\n')
                 quit()
             elif num == 2:
-                pass        # check word validity
+                pass  # check word validity
